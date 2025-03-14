2fe698322f036124dfa9b36f8f500d6db9ceaab3
ze674 <dastin77@icloud.com>
2025-03-15 00:33:17 +0300

    Добавлен label к input в форме  для доступности

--- templates_templ.go
+++ templates_templ.go
@@ -87,7 +87,7 @@ func productList(products []Product) templ.Component {
 			templ_7745c5c3_Var3 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "<div><h1>Типы продукции</h1><form action=\"/products\" method=\"post\"><input type=\"text\" name=\"name\" placeholder=\"Название\" required> <input type=\"text\" name=\"gtin\" placeholder=\"GTIN\" required> <button type=\"submit\">Добавить</button></form><ul>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 3, "<div><h1>Типы продукции</h1><form action=\"/products\" method=\"post\"><label for=\"name\">Название:</label> <input type=\"text\" id=\"name\" name=\"name\" placeholder=\"Название\" required> <label for=\"gtin\">GTIN:</label> <input type=\"text\" id=\"gtin\" name=\"gtin\" placeholder=\"GTIN\" required> <button type=\"submit\">Добавить</button></form><ul>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
@@ -99,7 +99,7 @@ func productList(products []Product) templ.Component {
 			var templ_7745c5c3_Var4 string
 			templ_7745c5c3_Var4, templ_7745c5c3_Err = templ.JoinStringErrs(p.Name)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `templates.templ`, Line: 27, Col: 27}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `templates.templ`, Line: 29, Col: 27}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var4))
 			if templ_7745c5c3_Err != nil {
@@ -112,7 +112,7 @@ func productList(products []Product) templ.Component {
 			var templ_7745c5c3_Var5 string
 			templ_7745c5c3_Var5, templ_7745c5c3_Err = templ.JoinStringErrs(p.GTIN)
 			if templ_7745c5c3_Err != nil {
-				return templ.Error{Err: templ_7745c5c3_Err, FileName: `templates.templ`, Line: 27, Col: 38}
+				return templ.Error{Err: templ_7745c5c3_Err, FileName: `templates.templ`, Line: 29, Col: 38}
 			}
 			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var5))
 			if templ_7745c5c3_Err != nil {
